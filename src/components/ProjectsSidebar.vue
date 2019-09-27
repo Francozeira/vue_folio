e127a14175cf2bb54e1a0060c5a1140efac428da
Leonardo Franco <leofranco1305@gmail.com>
2019-09-26 23:11:17 -0300

    Added AXIOS and github request and Sidebar data

--- src/components/ProjectsSidebar.vue
+++ src/components/ProjectsSidebar.vue
@@ -1,11 +1,48 @@
 <template>
-  <div
-    class="projectsSidebar test bg-teal-500 block w-full md:w-1/3 h-screen"
-  ></div>
+  <div class="projectsSidebar test bg-teal-900 block w-full md:w-1/3 h-screen">
+    <div class="pt-24 ml-12">
+      <div class="bg-white h-48 w-48 rounded">
+        <img
+          :src="userInfo.avatar_url"
+          class="p-2 rounded-full"
+          alt="User image"
+        />
+      </div>
+      <div class="pt-8 text-2xl text-left font-bold">{{ userInfo.name }}</div>
+      <div class="text-lg text-left font-bold">@{{ userInfo.login }}</div>
+      <div class="pt-8 text-left font-bold text-sm">
+        <span class="pr-1 fas fa-address-card"></span>
+        {{ userInfo.bio }}
+      </div>
+      <div class="pt-3 text-left font-bold text-sm">
+        <span class="pr-1 fas fa-map-marker-alt"></span>
+        {{ userInfo.location }}
+      </div>
+      <div class="pt-3 text-left font-bold text-sm">
+        <span class="pr-1 fas fa-briefcase"></span>
+        {{ userInfo.company }}
+      </div>
+      <div v-if="userInfo.hireable" class="pt-3 text-left font-bold text-sm">
+        <span class="pr-1 fas fa-link"></span>Hireable
+      </div>
+    </div>
+
+    <div class="w-3/4 mx-auto">
+      <div class="h-8 p-8 fa-2x fab fa-linkedin"></div>
+      <div class="h-8 p-8 fa-2x fab fa-instagram"></div>
+      <div class="h-8 p-8 fa-2x fab fa-github-square"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    userInfo: {
+      required: true
+    }
+  }
+};
 </script>
 
 <style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="projects">
-    <ProjectsSidebar />
-    <div class="inline-block w-full md:w-2/3">
-      <div>This is an about page</div>
-    </div>
+    <ProjectsSidebar :userInfo="userInfo" />
   </div>
 </template>
 
@@ -12,8 +9,29 @@ import ProjectsSidebar from "@/components/ProjectsSidebar.vue";
 export default {
   components: {
     ProjectsSidebar
+  },
+
+  data() {
+    return {
+      userInfo: []
+    };
+  },
+
+  beforeMount() {
+    fetch("https://api.github.com/users/francozeira")
+      .then(response => response.json())
+      .then(json => {
+        this.userInfo = json;
+        console.log(this.userInfo);
+      });
+    // .catch(console.log("Something went wrong with Github API info"))
   }
 };
 </script>
 
-<style></style>
+<style>
+.projects {
+  font-family: "Poppins", sans-serif;
+  color: white;
+}
+</style>
